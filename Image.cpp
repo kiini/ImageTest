09307cefd6b2c4bf78a061c59419c2f477cd1fd4
bibi <bibi.kjartans@gmail.com>
2019-10-25 13:40:30 +0100

    added user ctor and allication pixel buffer

--- Image.cpp
+++ Image.cpp
@@ -1,6 +1,20 @@
 
 #include "Image.h"
 
+Image::Image(unsigned int _width, unsigned int _height, unsigned int _channels)
+{
+    m_width=_width;
+    m_height=_height;
+    m_channels=_channels;
+    m_pixels= std::make_unique<unsigned char []>(m_width);
+}
+
+unsigned char * Image::pixels()
+{
+    return m_pixels.get();
+
+}
+
 unsigned int Image::width() const noexcept
 {
 
