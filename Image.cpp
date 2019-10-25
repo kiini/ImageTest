8fcb13072f66cf3824bffac31c5268d38027fb8a
bibi <bibi.kjartans@gmail.com>
2019-10-25 13:09:35 +0100

    working default ctor

--- Image.cpp
+++ Image.cpp
@@ -1,2 +1,21 @@
 
 #include "Image.h"
+
+unsigned int Image::width() const noexcept
+{
+
+    return m_width;
+
+}
+
+unsigned int Image::height() const noexcept
+{
+
+    return m_height;
+}
+
+unsigned int Image::channels() const noexcept
+{
+
+    return m_channels;
+}
